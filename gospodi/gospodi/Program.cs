72df5791c9365625deb4bc6af74c730ceddd1f66
j <112891364+jhellsing@users.noreply.github.com>
2022-09-19 20:57:54 +0300

    Add files via upload

---
+++ gospodi/gospodi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace gospodi
+{
+    class Program
+    {
+        static ServerObject server; // сервер
+        static Thread listenThread; // потока для прослушивания
+        static void Main(string[] args)
+        {
+           server = new ServerObject();
+           listenThread = new Thread(new ThreadStart(server.Listen));
+           listenThread.Start(); //старт потока
+        }
+    }
+}
